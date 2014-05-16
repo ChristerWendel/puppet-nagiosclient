8c7444b8cf4ce5626b9a8389ba1b4412afa68ebd
Christer Wendel <christer.wendel@altrusoft.se>
2014-05-16 09:35:25 +0200

    Added lm-sensors configurations

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,15 @@
 # == Class: nagiosclient::install
 #
-# Installs nagios-nrpe-server
+# Installs nagios-nrpe-server, and lm-sensors on physical hosts
 class nagiosclient::install {
   package { 'nagios-nrpe-server':
     ensure => $nagiosclient::version,
   }
+
+  @package { 'lm-sensors':
+    ensure => present,
+    tag    => 'physical',
+  }
+
+  Package <| tag == $::virtual |>
 }
